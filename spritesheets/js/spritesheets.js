e512ee85a57a6dfd59d3c56076a567c8ae282ac1
Andre Venancio <venancio.andre@gmail.com>
2013-07-10 16:49:34 +0100

    test

--- spritesheets/js/spritesheets.js
+++ spritesheets/js/spritesheets.js
@@ -11,23 +11,32 @@ var Spritesheets = function() {
   this.debug = new Razor.Debugger(true, false);
 
   this.folder = this.debug.gui.addFolder('Spritesheets');
+  this.folderCamping = this.folder.addFolder('Camping');
+  this.folderCinema = this.folder.addFolder('Cinema');
   this.folderConvertible = this.folder.addFolder('Convertible');
   this.folderPark = this.folder.addFolder('Park');
   this.debug.gui.open();
 
 
   this.bindedRender = this.render.bind(this);
+  this.bindedUpdateCamping = this.onUpdateCamping.bind(this);
+  this.bindedUpdateCinema = this.onUpdateCinema.bind(this);
   this.bindedUpdateConvertible = this.onUpdateConvertible.bind(this);
   this.bindedUpdatePark = this.onUpdatePark.bind(this);
 
   // the div where we render the animation
+  this.camping = document.getElementById('camping');
+  this.cinema = document.getElementById('cinema');
   this.convertible = document.getElementById('convertible');
   this.park = document.getElementById('park');
 
   // an instance of the Timeline class
+  this.timelineCamping = new Timeline(this.camping, 'spritesheets/camping.png', 13, 49, this.bindedUpdateCamping);
+  this.timelineCinema = new Timeline(this.cinema, 'spritesheets/cinema.png', 13, 49, this.bindedUpdateCinema);
   this.timelineConvertible = new Timeline(this.convertible, 'spritesheets/convertible.png', 13, 49, this.bindedUpdateConvertible);
   this.timelinePark = new Timeline(this.park, 'spritesheets/park.png', 30, 81, this.bindedUpdatePark);
 
+  this.currentFrame = 0;
   this.init();
 };
 
@@ -40,13 +49,20 @@ Spritesheets.prototype.init = function() {
   this.folder.add(this, 'startLooping');
   this.folder.add(this, 'stopLooping');
   this.folder.open();
+
+  this.folderCamping.add(this.timelineCamping, 'currentFrame').listen();
+  this.folderCamping.open();
+
+  this.folderCinema.add(this.timelineCinema, 'currentFrame').listen();
+  this.folderCinema.open();
+
   this.folderConvertible.add(this.timelineConvertible, 'currentFrame').listen();
   this.folderConvertible.open();
 
   this.folderPark.add(this.timelinePark, 'currentFrame').listen();
   this.folderPark.open();
 
-  this.startLooping();
+  //this.startLooping();
   requestAnimationFrame(this.bindedRender);
 };
 
@@ -57,18 +73,35 @@ Spritesheets.prototype.init = function() {
 Spritesheets.prototype.render = function() {
   this.debug.begin();
 
+  this.camping.className = 'camping camping_frame_' + this.currentFrame % 10;
+  this.cinema.className = 'cinema cinema_frame_' + this.currentFrame % 10;
+  this.convertible.className = 'convertible convertible_frame_' + this.currentFrame % 10;
+  this.park.className = 'park park_frame_' + this.currentFrame % 10;
+
+  this.currentFrame++;
   this.debug.end();
   requestAnimationFrame(this.bindedRender);
 };
 
 
 Spritesheets.prototype.startLooping = function() {
+  this.timelineCamping.start();
+  this.timelineCinema.start();
   this.timelineConvertible.start();
   this.timelinePark.start();
 };
 
 Spritesheets.prototype.stopLooping = function() {
   var scope = this;
+
+  this.timelineCamping.end(function() {
+    console.log('animation', scope.timelineCamping.id, "ended");
+  });
+
+  this.timelineCinema.end(function() {
+    console.log('animation', scope.timelineCinema.id, "ended");
+  });
+
   this.timelineConvertible.end(function() {
     console.log('animation', scope.timelineConvertible.id, "ended");
   });
@@ -76,6 +109,14 @@ Spritesheets.prototype.stopLooping = function() {
   this.timelinePark.end(function() {
     console.log('animation', scope.timelinePark.id, "ended");
   });
+};
+
+Spritesheets.prototype.onUpdateCamping = function(currentFrame) {
+  this.camping.className = 'camping camping_frame_' + currentFrame
+};
+
+Spritesheets.prototype.onUpdateCinema = function(currentFrame) {
+  this.cinema.className = 'cinema cinema_frame_' + currentFrame
 };
 
 Spritesheets.prototype.onUpdateConvertible = function(currentFrame) {
